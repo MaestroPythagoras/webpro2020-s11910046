63efaaa0825bbd94a2a3a30007a5a871afcd1852
MaestroPythagoras <maestro.pythagoras@gmail.com>
2020-09-10 18:25:55 +0700

    Update: Tugas & Latihan Pertemuan_07

---
+++ Pertemuan_7/index.js
@@ -0,0 +1,35 @@
+/* 
+1. JavaScript Conditional Statements
+*/
+var satu = prompt("Masukkan angka pertama");
+		var two = prompt("Masukkan angka kedua");
+		satu = parseInt(satu);
+		dua = parseInt(dua);
+		if (one == two)
+			document.write(one + " adalah sama dengan " + dua + ".");
+		else if (one<two)
+			document.write(one + " kurang dari " + dua + ".");
+		else
+            document.write(one + " lebih besar dari " + dua + ".");
+            
+/* 
+2. JavaScript Loop
+*/
+
+var i;
+for (i = 0; i < cars.length; i++) {
+  text += cars[i] + "<br>";
+}
+
+/*
+Output:
+
+text += cars[0] + "<br>";
+text += cars[1] + "<br>";
+text += cars[2] + "<br>";
+text += cars[3] + "<br>";
+text += cars[4] + "<br>";
+text += cars[5] + "<br>";
+*/
+
+
