489a8aaf4cf1513173197bae08cf30e9129b5f7c
MaestroPythagoras <maestro.pythagoras@gmail.com>
2020-09-29 13:23:16 +0700

    Update: Latihan pertemuan_12 & Exercise pertemuan_11

--- pertemuan_11/index.js
+++ pertemuan_11/index.js
@@ -13,3 +13,28 @@ const operasiPenjumlahan = (bilangan1, bilangan2) => {
   }
   
   console.log(namaJenisAnjing());
+
+  // Exercise (Convert to ES6 Syntax)
+  // ES5
+  function yearUntilRentirement(year, firstName) {
+    var age = 28;
+    var retirement = 65 - age;
+    if (retirement > 0) {
+      console.log(firstName + " retired in " + retirement + " years");
+    }
+    else {
+      console.log(firstName + " is already retired.");
+    }
+  }
+
+  // ES6 Syntax
+  function yearUntilRentirement(year, firstName) {
+    const age = 28;
+    const retirement = 65 - age;
+    if (retirement > 0) {
+      console.log(`${firstName} retired in ${retirement} years`);
+    }
+    else {
+      console.log(`${firstName} is already retired.`);
+    }
+  }
