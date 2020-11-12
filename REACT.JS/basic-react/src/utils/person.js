da0a7dd113761013ba8a6255432b766aaf1535a0
MaestroPythagoras <maestro.pythagoras@gmail.com>
2020-11-12 21:19:43 +0700

    Update: Props

---
+++ REACT.JS/basic-react/src/utils/person.js
@@ -0,0 +1,20 @@
+export const person = [
+    {
+      name: "Maestro Pythagoras",
+      institution: "Universitas Klabat",
+      address: "Bintaro - Jakarta Selatan",
+      phonenumber: "081388505689",
+    },
+    {
+      name: "John Doe",
+      institution: "Universitas Klabat",
+      address: "Bintaro - Jakarta Selatan",
+      phonenumber: "081388505689",
+    },
+    {
+      name: "Kelvin Setiawan",
+      institution: "Universitas Klabat",
+      address: "Bintaro - Jakarta Selatan",
+      phonenumber: "081388505689",
+    },
+  ]
