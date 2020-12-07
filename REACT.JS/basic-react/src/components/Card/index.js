7efb9485834d30767547b0bbdb0301bde0322953
MaestroPythagoras <maestro.pythagoras@gmail.com>
2020-12-07 16:12:36 +0700

    Update: Pertemuan 24

--- REACT.JS/basic-react/src/components/Card/index.js
+++ REACT.JS/basic-react/src/components/Card/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = ({name, institution, address, phonenumber}) => {
+const Card = ({name, username, email, phone}) => {
     return (
         <div className="card-container">
             <p>{name}</p>
-            <p>{institution}</p>
-            <p>{address}</p>
-            <p>{phonenumber}</p>
+            <p>{username}</p>
+            <p>{email}</p>
+            <p>{phone}</p>
         </div>
     );
 };
